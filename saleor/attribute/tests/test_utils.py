KaiyaoZhang/max-cuba-saleor

Add model and API changes to the Product - Attribute relation

saleor/attribute/tests/test_utils.py
@@ -2,6 +2,7 @@ import pytest
 
 from ...product.models import ProductType
 from ..utils import associate_attribute_values_to_instance
+from .model_helpers import get_product_attribute_values, get_product_attributes
 
 
 def test_associate_attribute_to_non_product_instance(color_attribute):
@@ -33,29 +34,28 @@ def test_associate_attribute_to_product_instance_from_different_attribute(
 
 def test_associate_attribute_to_product_instance_without_values(product):
     """Ensure clearing the values from a product is properly working."""
-    old_assignment = product.attributes.first()
-    assert old_assignment is not None, "The product doesn't have attribute-values"
-    assert old_assignment.values.count() == 1
-
-    attribute = old_assignment.attribute
+    attribute = get_product_attributes(product).first()
+    assert attribute is not None, "Product doesn't have attributes assigned"
+    value_count = get_product_attribute_values(product, attribute).count()
+    assert value_count == 1, "Product doesn't have attribute-values"
 
     # Clear the values
-    new_assignment = associate_attribute_values_to_instance(product, attribute)
+    associate_attribute_values_to_instance(product, attribute)
 
     # Ensure the values were cleared and no new assignment entry was created
-    assert new_assignment.pk == old_assignment.pk
-    assert new_assignment.values.count() == 0
+    assert get_product_attributes(product).count() == 1
+    assert product.attributevalues.count() == 0
 
 
 def test_associate_attribute_to_product_instance_multiple_values(
     product, attribute_value_generator
 ):
     """Ensure multiple values in proper order are assigned."""
-    old_assignment = product.attributes.first()
-    assert old_assignment is not None, "The product doesn't have attribute-values"
-    assert old_assignment.values.count() == 1
+    attribute = get_product_attributes(product).first()
+    assert attribute is not None, "Product doesn't have attributes assigned"
+    value_count = get_product_attribute_values(product, attribute).count()
+    assert value_count == 1, "Product doesn't have attribute-values"
 
-    attribute = old_assignment.attribute
     attribute_value_generator(
         attribute=attribute,
         slug="attr-value2",
@@ -63,16 +63,14 @@ def test_associate_attribute_to_product_instance_multiple_values(
     values = attribute.values.all()
 
     # Assign new values
-    new_assignment = associate_attribute_values_to_instance(
-        product, attribute, values[1], values[0]
-    )
+    associate_attribute_values_to_instance(product, attribute, values[1], values[0])
 
     # Ensure the new assignment was created and ordered correctly
-    assert new_assignment.pk == old_assignment.pk
-    assert new_assignment.values.count() == 2
-    assert list(
-        new_assignment.productvalueassignment.values_list("value__pk", "sort_order")
-    ) == [(values[1].pk, 0), (values[0].pk, 1)]
+    assert product.attributevalues.count() == 2
+    assert list(product.attributevalues.values_list("value__pk", "sort_order")) == [
+        (values[1].pk, 0),
+        (values[0].pk, 1),
+    ]
 
 
 def test_associate_attribute_to_page_instance_multiple_values(page):

rohan-flutterint/saleor-core

saleor/attribute/tests/test_utils.py
@@ -2,6 +2,7 @@ import pytest
 
 from ...product.models import ProductType
 from ..utils import associate_attribute_values_to_instance
+from .model_helpers import get_product_attribute_values, get_product_attributes
 
 
 def test_associate_attribute_to_non_product_instance(color_attribute):
@@ -33,29 +34,28 @@ def test_associate_attribute_to_product_instance_from_different_attribute(
 
 def test_associate_attribute_to_product_instance_without_values(product):
     """Ensure clearing the values from a product is properly working."""
-    old_assignment = product.attributes.first()
-    assert old_assignment is not None, "The product doesn't have attribute-values"
-    assert old_assignment.values.count() == 1
-
-    attribute = old_assignment.attribute
+    attribute = get_product_attributes(product).first()
+    assert attribute is not None, "Product doesn't have attributes assigned"
+    value_count = get_product_attribute_values(product, attribute).count()
+    assert value_count == 1, "Product doesn't have attribute-values"
 
     # Clear the values
-    new_assignment = associate_attribute_values_to_instance(product, attribute)
+    associate_attribute_values_to_instance(product, attribute)
 
     # Ensure the values were cleared and no new assignment entry was created
-    assert new_assignment.pk == old_assignment.pk
-    assert new_assignment.values.count() == 0
+    assert get_product_attributes(product).count() == 1
+    assert product.attributevalues.count() == 0
 
 
 def test_associate_attribute_to_product_instance_multiple_values(
     product, attribute_value_generator
 ):
     """Ensure multiple values in proper order are assigned."""
-    old_assignment = product.attributes.first()
-    assert old_assignment is not None, "The product doesn't have attribute-values"
-    assert old_assignment.values.count() == 1
+    attribute = get_product_attributes(product).first()
+    assert attribute is not None, "Product doesn't have attributes assigned"
+    value_count = get_product_attribute_values(product, attribute).count()
+    assert value_count == 1, "Product doesn't have attribute-values"
 
-    attribute = old_assignment.attribute
     attribute_value_generator(
         attribute=attribute,
         slug="attr-value2",
@@ -63,16 +63,14 @@ def test_associate_attribute_to_product_instance_multiple_values(
     values = attribute.values.all()
 
     # Assign new values
-    new_assignment = associate_attribute_values_to_instance(
-        product, attribute, values[1], values[0]
-    )
+    associate_attribute_values_to_instance(product, attribute, values[1], values[0])
 
     # Ensure the new assignment was created and ordered correctly
-    assert new_assignment.pk == old_assignment.pk
-    assert new_assignment.values.count() == 2
-    assert list(
-        new_assignment.productvalueassignment.values_list("value_id", "sort_order")
-    ) == [(values[1].pk, 0), (values[0].pk, 1)]
+    assert product.attributevalues.count() == 2
+    assert list(product.attributevalues.values_list("value_id", "sort_order")) == [
+        (values[1].pk, 0),
+        (values[0].pk, 1),
+    ]
 
 
 def test_associate_attribute_to_page_instance_multiple_values(page):